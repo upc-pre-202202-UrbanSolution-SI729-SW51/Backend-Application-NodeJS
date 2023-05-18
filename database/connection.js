d92f920739984c92a050405ace07c793120a8f04
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2023-05-17 20:41:57 -0500

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connection = async() => {
     try {
 
-        await mongoose.connect("mongodb://mongo:mcoX6awOZuQ5r5G1cMwS@containers-us-west-151.railway.app:6987/");
+        await mongoose.connect("mongodb://mongo:3t0moHH35fWEvGfTu46h@containers-us-west-41.railway.app:6051/");
         console.log("Connected to the api_rest_social_media database");
 
     } catch(error) {
