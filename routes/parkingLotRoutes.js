541d7a93a28143e7ff486545cc26bfa50f3c55cf
u201910146 <85463913+JoseR200@users.noreply.github.com>
2023-06-12 19:28:22 -0500

    updated endpoint for flutter development

--- routes/parkingLotRoutes.js
+++ routes/parkingLotRoutes.js
@@ -7,9 +7,11 @@ router.get("/list", check.auth, ParkingLotController.list);
 router.post("/", check.auth, ParkingLotController.create);
 router.put("/:id", ParkingLotController.editParkingLot);
 router.get("/:id", check.auth, ParkingLotController.parkingLotById);
-router.delete("/:id", check.auth, ParkingLotController.deleteParkingLot);
+
 //Endpoits for flutter mobile app v1
 router.get("/owner/:id", ParkingLotController.listFromOwner);
 router.post("/:idOwner", ParkingLotController.createByOwnerId);
+//Endpoits without auth for flutter mobile app v1 (return auth when finished)
+router.delete("/:id", ParkingLotController.deleteParkingLot);
 
 module.exports = router;
